b4d885d8007d573664fc3deb077b280f805c9124
Lucas Mah <lucasmah@outlook.com>
2024-06-01 09:57:21 -0700

    Base Design Complete

--- components/header.jsx
+++ components/header.jsx
@@ -1,30 +1,24 @@
-import Image from 'next/image';
 import Link from 'next/link';
-import netlifyLogo from 'public/netlify-logo.svg';
-import githubLogo from 'public/images/github-mark-white.svg';
+import Image from 'next/image';
 
 const navItems = [
-    { linkText: 'Home', href: '/' },
-    { linkText: 'Revalidation', href: '/revalidation' },
-    { linkText: 'Image CDN', href: '/image-cdn' },
-    { linkText: 'Edge Function', href: '/edge' },
-    { linkText: 'Blobs', href: '/blobs' },
-    { linkText: 'Classics', href: '/classics' }
+    { linkText: 'About', href: '/#about' },
+    { linkText: 'Experience', href: '/#experience' },
+    { linkText: 'Projects', href: '/#projects' },
+    { linkText: 'Contact', href: '/#contact' }
 ];
 
 export function Header() {
     return (
-        <nav className="flex flex-wrap items-center gap-4 pt-6 pb-12 sm:pt-12 md:pb-24">
-            <Link href="/">
-                <Image src={netlifyLogo} alt="Netlify logo" />
-            </Link>
+        <nav className="header flex w-[100%] justify-between m-auto p-[15px] px-14">
+            <Image src={'/name.png'} height={30} width={150} alt='logo'/>
             {!!navItems?.length && (
-                <ul className="flex flex-wrap gap-x-4 gap-y-1">
+                <ul className="flex gap-[20px]">
                     {navItems.map((item, index) => (
                         <li key={index}>
                             <Link
                                 href={item.href}
-                                className="inline-block px-1.5 py-1 transition hover:opacity-80 sm:px-3 sm:py-2"
+                                className="inline-block hover:opacity-80 sm:px-3 no-underline"
                             >
                                 {item.linkText}
                             </Link>
@@ -32,15 +26,6 @@ export function Header() {
                     ))}
                 </ul>
             )}
-            <div className="flex-grow justify-end hidden lg:flex lg:mr-1">
-                <Link
-                    href="https://github.com/netlify-templates/next-platform-starter"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image src={githubLogo} alt="GitHub logo" className="w-7" />
-                </Link>
-            </div>
         </nav>
     );
 }
