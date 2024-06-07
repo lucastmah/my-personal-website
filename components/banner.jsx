13f5a632934134a1020953722147ad0ee10f23db
Lucas Mah <lucasmah@outlook.com>
2024-06-07 12:05:46 -0700

    fixed build issue

--- components/banner.jsx
+++ components/banner.jsx
@@ -2,7 +2,7 @@ export function Banner() {
     return (
         <div id='banner'>
             <h5>Watch your head!</h5>
-            <p>This page is under construction. If you see this banner, you're special! You're here early and get to see this website in its rawest form. :)</p>
+            <p>This page is under construction. If you see this banner, you're special! You're here early and get to see this website in its rawest form.</p>
         </div>
     );
 }
